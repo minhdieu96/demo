bf84ebf8226c7f2d138d352aa9f48aac4be72a7a
minhdieu96 <nifanqin@gmail.com>
2023-04-09 18:07:19 +0700

    commit test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,49 @@
 package org.example;
 
 public class Main {
+    static int toanCuc= 0;
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Hello Minhdieu!");
+//        System.out.println("Tong hai so tu nhien la: ");
+//        System.out.println(plusTwoNumber(12,2));
+//        System.out.println("Hieu hai so tu nhien la: ");
+//        System.out.println(minusTwoNumber(12,2));
+        System.out.println("Tong ba so tu nhien la: ");
+        System.out.println(plusThreeNumber(12,2,3));
+        System.out.println(showName1("Dieu", "Quang Tri"));
+        showName("Loan");
+
+    }
+    /*
+        Day la method tinh tong hai so tu nhien
+        huhiohjhjohj
+     */
+    public static int plusTwoNumber(int num1, int num2){
+        // Tong hai so tu nhien = num1 + num2
+        int sum = num1 + num2;
+        toanCuc = sum;
+        // Tra ve ket qua
+        return sum;
+    }
+    // tru hai so tu nhien = num1 - num2
+    public static int minusTwoNumber(int num1, int num2){
+        int minus = num1 - num2;
+        // Tra ve ket qua
+        return minus;
+    }
+    public static int plusThreeNumber (int num1, int num2, int num3){
+        int sum = num1 + num2 + num3;
+       // return sum;
+        return sum;
+    }
+    private static void showName(String name){
+        String a = "Her name is: ";
+        System.out.println(a+name);
+    }
+
+    private static String showName1(String name, String city){
+        String a = "Her name is: " + name + " and she from: " + city;
+        return a;
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Student {
+    int name;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+}
